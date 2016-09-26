91eb96235511a0b58cd754255c4f014a5e796127
Brian Boyko <brian.boyko@gmail.com>
2016-09-26 03:51:20 -0500

    Finally have it to where it works.  Ready for next step

--- src/parser.js
+++ src/parser.js
@@ -7,95 +7,91 @@ import csv from 'csv-to-json';
 // "Antigonish, Subd. B"
 // Use an off-the rack parser instead of this one, should fix the data problems.
 
-const readFile = (dirname, filename) => new Promise(function(resolve, reject) {
-    fs.readFile(dirname + filename, 'utf-8', (err, content) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve({filename, content})
-    });
-});
-
 const writeFile = (filename, content) => new Promise(function(resolve, reject) {
-    fs.appendFile("./out/" + filename + ".json", JSON.stringify(content, null, 2), (err) => {
-        if (err) {
-            reject(err);
-        } else {
-            resolve(true);
-        }
-    })
-});
-
-const readNWrite = ({dirname, filename}) => new Promise(function(resolve, reject) {
-    readFile(dirname, filename)
-      .then(({file, content}) => writeFile(file, content))
-      .then(resolve).catch((x) => reject(x))
-});
-
-const getFileNames = (dirname) => new Promise(function(resolve, reject) {
-    fs.readdir(dirname, (err, filenames) => {
-        if (err) {
-            reject(err);
-            return;
-        }
-        resolve(filenames);
-    })
-});
-
-const stagger = (arrayOfArgs, thenable) => new Promise(function(resolve, reject) {
-    if (arrayOfArgs.length <= 0) {
-        resolve(true)
-        return;
-    }
-    let file = arrayOfArgs.shift();
-    thenable(file).then((successEh) => {
-        if (!successEh) {
-            reject("Error: File did not successfully write");
-        } else {
-            stagger(arrayOfArgs, thenable).then(() => resolve())
-        }
-    })
-});
-
-const parseCSV = (dir, filename) => new Promise(function(resolve, reject) {
-  csv.parse({filename: dir + filename}, (err, json) => {
-    if(err){
+  fs.appendFile("./out/" + filename + ".json", JSON.stringify(content, null, 2), (err) => {
+    if (err) {
       reject(err);
+    } else {
+      resolve(true);
     }
-    resolve(json);
   })
 });
 
+const getFileNames = (dirname) => new Promise(function(resolve, reject) {
+  fs.readdir(dirname, (err, filenames) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(filenames);
+  })
+});
+
+// const stagger = (arrayOfArgs, thenable) => new Promise(function(resolve, reject) {
+//   if (arrayOfArgs.length <= 0) {
+//     resolve(true)
+//     return;
+//   }
+//   let file = arrayOfArgs.shift();
+//   thenable(file)
+//     .then((successEh) => {
+//       if (!successEh) {
+//         reject("Error: File did not successfully write");
+//       } else {
+//         stagger(arrayOfArgs, thenable)
+//           .then(() => resolve())
+//       }
+//     })
+// });
+
+const parseCSV = (dir, filename) => new Promise(function(resolve, reject) {
+  csv.parse({
+    filename: dir + filename
+  }, (err, json) => {
+    if (err) {
+      reject(err);
+    }
+    resolve({json, filename});
+  })
+});
+
+/**
+ * [countVotes description]
+ * @param  {[type]} lines [description]
+ * @return {[type]}       [description]
+ */
 const countVotes = (lines) => {
+  let re = /\"/g; // strip out quote literals
+  lines = _.filter(lines, (line) => !!line['﻿Electoral District Number/Numéro de circonscription'])
   let initialInfo = {
-    districtNumber: lines[0]['﻿Electoral District Number/Numéro de circonscription'],
-    districtNameEnglish: lines[0]['Electoral District Name_English/Nom de circonscription_Anglais'],
-    districtNameFrench: lines[0]['Electoral District Name_French/Nom de circonscription_Français'],
+    districtNumber: lines[0]['﻿Electoral District Number/Numéro de circonscription'].replace(re, ''),
+    districtNameEnglish: lines[0]['Electoral District Name_English/Nom de circonscription_Anglais'].replace(re, ''),
+    districtNameFrench: lines[0]['Electoral District Name_French/Nom de circonscription_Français'].replace(re, ''),
   }
+  console.log(initialInfo)
   let simplifiedLines = lines.map((line) => ({
-    firstName: line['Candidate’s First Name/Prénom du candidat'],
-    familyName: line['Candidate’s Family Name/Nom de famille du candidat'],
-    middleName: line['Candidate’s Middle Name/Second prénom du candidat'],
-    party: line['Political Affiliation Name_English/Appartenance politique_Anglais'],
+    firstName: line['Candidate’s First Name/Prénom du candidat'].replace(re, ''),
+    familyName: line['Candidate’s Family Name/Nom de famille du candidat'].replace(re, ''),
+    middleName: line['Candidate’s Middle Name/Second prénom du candidat'].replace(re, ''),
+    party: line['Political Affiliation Name_English/Appartenance politique_Anglais'].replace(re, ''),
     votes: parseInt(line['Candidate Poll Votes Count/Votes du candidat pour le bureau']),
   }))
   return simplifiedLines.reduce((pv, line) => {
-    if(line.familyName === undefined){
+    if (!line.familyName) {
       return pv;
     }
-    if(line.party === "No Affiliation" || line.party === "Independent"){
-      if(!pv.hasOwnProperty("Independents")){
+    if (line.party === "No Affiliation" || line.party === "Independent") {
+      if (!pv.Independents) {
         pv.Independents = {}
       }
-      if(!pv.Independents.hasOwnProperty(line.familyName)){
+      if (!pv.Independents[line.familyName]) {
         pv.Independents[line.familyName] = _.pick(line, ["familyName", "middleName", "firstName"])
         pv.Independents[line.familyName].votes = 0;
       }
       pv.Independents[line.familyName].votes += line.votes;
       return pv;
     }
-    if(!pv.hasOwnProperty[line.party]){
+    if (!pv[line.party]) {
       pv[line.party] = _.pick(line, ["familyName", "middleName", "firstName"]);
       pv[line.party].votes = 0;
     }
@@ -104,14 +100,13 @@ const countVotes = (lines) => {
   }, initialInfo)
 }
 
-const getAllFiles = (dirname) => new Promise(function(resolve, reject) {
-    getFileNames(dirname)
-      .then((filenames) => Promise.all(filenames.map((filename) => parseCSV(dirname, filename)))
-      .then((jsons) => jsons.map((json) => countVotes(json))))
-      .then((jsons) => resolve(jsons))
-      .catch((err) => reject(err))
-})
+
+const getAllFiles = (dirname) => {
+  getFileNames(dirname)
+    .then((filenames) => Promise.all(filenames.map((filename) => parseCSV(dirname, filename)))
+    .then((jsons) => jsons.map(({json, filename}) => ({filename: filename, votes: countVotes(json)}))))
+    .then((items) => Promise.all(items.map(({filename, votes}) => writeFile(filename, votes))))
+    .catch((err) => console.log(err))
+}
 
 getAllFiles('./src/data/byDistrict/')
-  .then((x) => console.log(x))
-  .catch((e) => console.log(e))
