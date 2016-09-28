330a9e9d1f2ceda18c520f6acb93dec3eb1e93e9
Brian Boyko <brian.boyko@gmail.com>
2016-09-28 18:29:41 -0500

    added writeFile() and parseCSV() tests

--- test/utils/spec_readJson.js
+++ test/utils/spec_readJson.js
@@ -17,11 +17,12 @@ const spyObj = {
 };
 
 import readJson from '../../src/utils/readJson';
+import parseCSV_expected from '../examples/parseCSV_expected';
 
 let {
   getFileNames,
-  // writeFile,
-  // parseCSV,
+  writeFile,
+  parseCSV,
   // stripQuotes,
   // consolidatePollingPlaces,
   // countVotes,
@@ -33,6 +34,19 @@ describe('/utils/readJson.js', function(){
     it('should call fs.readdir with the correct directory name', function(){
       getFileNames("wockaWocka!");
       expect(rd.getCall(0).args[0]).to.equal("wockaWocka!");
+    });
+  });
+  describe('writeFile()', function(){
+    it('should write JSON to a file', function(){
+      const testObj = {you: ["must", "build", "additional", 3.14, "lons"]};
+      writeFile("test", testObj);
+      expect(appF.getCall(0).args[0]).to.equal("./out/test.json");
+      expect(appF.getCall(0).args[1]).to.equal(JSON.stringify(testObj, null, 2));
+    });
+  });
+  describe('parseCSV', function(){
+    it('should convert CSV to JSON', function(done){
+      expect(parseCSV('./test/examples/', 'testcsv.csv')).to.eventually.eql({filename: 'testcsv.csv', json: parseCSV_expected}).notify(done);
     });
   });
 });
