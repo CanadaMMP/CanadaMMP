8c8a8330421d429afe2858ca626e9aa9f19caae8
Brian Boyko <brian.boyko@gmail.com>
2016-10-04 19:21:14 -0500

    Safety commit while I boot to Windows

--- src/frontend/js/containers/Hero.js
+++ src/frontend/js/containers/Hero.js
@@ -1,22 +1,23 @@
 import reduxify from '../utilities/reduxify';
 import * as actions from '../actions/index';
 import React, {Component} from 'react';
+import ReactDOM, {findDOMNode} from 'react-dom';
 import _ from 'lodash';
 import palette from '../constants/palette';
 import Paper from 'material-ui/Paper';
 
 const headline = {
-  English: "What you need to know about MMP\nand proportional representation",
-  Français: "Ce que vous devez savoir sur MMP\n et la représentation proportionnelle"
+  English: "What you need to know about MMP and proportional representation",
+  Français: "Ce que vous devez savoir sur MMP et la représentation proportionnelle"
 };
 
 const subheadline = {
   English: ["In a short time, Canada will be asked to choose whether or not they will adopt a new, more proportional, representation system. It's a new way of voting and thinking about representation.",
     "We think that the choice to go with MMP is the smart one for Canada, and in this interactive demo, we hope to explain why MMP will allow voters to have more say in their government, and better hold politicians accountable.",
-    "Mixed Member Proportional is simply a fairer, better system for electing representatives to Parliament. Here's why:"],
+    "Mixed Member Proportional is simply a fairer, better system for electing representatives to Parliament.", "Here's why:"],
   Français: ["Dans un court laps de temps, le Canada sera demandé de choisir si oui ou non ils vont adopter un nouveau système de représentation, plus proportionnel. Il est une nouvelle façon de voter et la réflexion sur la représentation.",
     "Nous pensons que le choix d'aller avec MMP est une puce pour le Canada, et dans cette démo interactive, nous espérons expliquer pourquoi MMP permettra aux électeurs d'avoir plus disent dans leur gouvernement, et de meilleurs politiciens de tenir responsables.",
-    "Proportionnelle mixte est tout simplement un, meilleur système plus juste pour l'élection des représentants au Parlement. Voici pourquoi:"],
+    "Proportionnelle mixte est tout simplement un, meilleur système plus juste pour l'élection des représentants au Parlement.", "Voici pourquoi:"],
 };
 
 const definitions = {
@@ -34,10 +35,27 @@ const definitions = {
   }
 };
 
+const leftSidebarText = {
+  English: ["In the 2015 Federal Election",
+    "70%",
+    "of votes",
+    "had literally",
+    "zero",
+    "influence on the makeup",
+    "of Parliament"],
+  Français: ["In the 2015 Federal Election",
+    "70%",
+    "of votes",
+    "had literally",
+    "nul",
+    "influence on the makeup",
+    "of Parliament"],
+};
+
 const heroStyle = {
   // height: '100px',
   width: '100%',
-  textAlign: 'left',
+  textAlign: 'center',
   display: 'inline-block',
   backgroundColor: 'rgba(0,0,0,0.7)',
 };
@@ -45,25 +63,50 @@ const heroStyle = {
 const headlineStyle = {
   color: 'white',
   fontFamily: "Patua One",
-  margin: '5vh 10vw 10vh 10vw',
+  margin: '5vh 10vw',
   fontSize: '3em'
 };
 
+const flexContainer = {
+  display: "flex",
+  flexDirection: "row",
+  justifyContent: "space-around",
+  alignItems: "stretch",
+  marginBottom: '5vh',
+};
+const leftContainer = {
+  order: "1",
+  color: "white",
+  fontFamily: "Patua One",
+  minWidth: "25%",
+};
+const rightContainer = {
+  order: "2",
+  flex: "flex-grow",
+  textAlign: 'left',
+  maxWidth: '800px',
+};
 const subheadStyle={
   fontFamily: "Lato",
-  margin: '5vh 10vw 5vh 10vw',
+  // margin: '5vh 10vw 5vh 10vw',
   padding: '3vh',
   fontSize: '1em'
 };
 const subContainerStyle={
   fontFamily: "Lato",
-  margin: '5vh 10vw 5vh 10vw',
-  padding: '3vh',
-  fontSize: '1em'
+  marginTop: "3vh",
+  fontSize: '1.1em',
+  backgroundColor: 'rgba(255,255, 255,0.7)',
 }
 class Hero extends Component {
   constructor(props){
     super(props);
+    this.componentDidMount = this.componentDidMount.bind(this);
+  }
+  componentDidMount() {
+    let thing = findDOMNode(this.refs.leftContainer)
+    console.log(thing);
+    console.log(thing.offsetWidth);
   }
 
   render () {
@@ -72,11 +115,17 @@ class Hero extends Component {
         <div style={headlineStyle}>
           {headline[this.props.language]}
         </div>
-        <Paper style={subContainerStyle} zDepth={3}>
-          <div style={subheadStyle}>
-          {subheadline[this.props.language].map((line) => <div style={{marginBottom: '1vh'}}>{line}</div>)}
-          </div>
-        </Paper>
+        <div style={flexContainer}>
+        <div ref="leftContainer" style={leftContainer}>
+        </div>
+        <div style={rightContainer}>
+          <Paper style={subContainerStyle} zDepth={3}>
+            <div style={subheadStyle}>
+            {subheadline[this.props.language].map((line) => (<div style={{marginBottom: "1vh"}}>{line}</div>))}
+            </div>
+          </Paper>
+        </div>
+        </div>
       </Paper>
       </div>
     );
